69f2c19f3e9c192ebfd2f72e8482a0baf7e3c628
ianzyvith <izyvith25@gmail.com>
2022-08-04 22:35:38 -0400

    added sequalize

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -50,7 +50,7 @@ router.get('/:id', (req, res) => {
         res.status(404).json({ message: 'No product found with this id' });
         return;
       }
-      res.json(dbUserData);
+      res.json(dbProductData);
     })
     .catch(err => {
       console.log(err);
